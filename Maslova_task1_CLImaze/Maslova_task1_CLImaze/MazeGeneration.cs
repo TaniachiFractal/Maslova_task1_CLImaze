1752d3367969415d1a70054f058a74206eaaea28
TaniachiFractal <tnmslv@yandex.ru>
2024-09-15 19:34:21 +0300

    Add editorconfig and MazeGeneration.cs

---
+++ Maslova_task1_CLImaze/Maslova_task1_CLImaze/MazeGeneration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maslova_task1_CLImaze
+{
+    /// <summary>
+    /// Class for generating the maze
+    /// </summary>
+    /// <remarks>Класс для генерации лабиринта</remarks>
+    internal class MazeGeneration
+    {
+    }
+}
